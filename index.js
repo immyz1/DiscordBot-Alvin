40cfd8b6382df12f301abf7bb0ae0f2de76f46ed
Immyz <imranu@patriot1tech.com>
2022-04-07 20:43:09 -0400

    initialized node app

---
+++ index.js
@@ -0,0 +1,9 @@
+//import Client library and Intents library from discord.js
+const { Client, Intents } = require['discord.js']
+const client = new Client({ Intents: [Intents.FLAGS.GUILDS]});
+
+client.once('ready', () = {
+    console.console.log();
+})
+
+client.login("OTYxNDcyNTczMDgwNzM5ODYw.Yk5fHA.ERLlDmIJ_Q7HqWf-czT5P-tJcto")
